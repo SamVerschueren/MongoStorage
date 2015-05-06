bd559e5687a351036589e225732e94d41707f216
Sam Verschueren <sam.verschueren@gmail.com>
2015-05-06 11:02:08 +0200

    Switched from grunt to gulp

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/**
+ * This gulp file describes some tasks to build the library.
+ *
+ * @author Sam Verschueren      <sam.verschueren@gmail.com>
+ * @since  6 Apr. 2015
+ */
+
+// module dependencies
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    header = require('gulp-header'),
+    sourcemaps = require('gulp-sourcemaps'),
+    del = require('del'),
+    moment = require('moment');
+
+var pkg = require('./package.json'),
+    banner = ['/**',
+              ' * <%= pkg.name %> v<%= pkg.version %>',
+              ' * ',
+              ' * @author <%= pkg.author.name %>     <<%= pkg.author.email %>>',
+              ' * @since  ' + moment().format('DD MMM YYYY'),
+              ' */\n'].join('\n');
+
+/**
+ * The clean tasks removes the dist folder so that a clean build
+ * can be done.
+ */
+gulp.task('clean', function(cb) {
+    del(['dist/'], cb);
+});
+
+/**
+ * The build task concatenates and uglifies everything.
+ */
+gulp.task('build', ['clean'], function() {
+    return gulp.src(['src/Collection.js', 'src/Collection.Modify.js', 'src/Collection.Selector.js', 'src/*.js', '!src/*.min.js'])
+        .pipe(sourcemaps.init())
+        .pipe(concat(pkg.name + '.min.js'))
+        .pipe(uglify())
+        .pipe(header(banner, {pkg: pkg}))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist'));
+});
